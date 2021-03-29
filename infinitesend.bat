05cc610c970defa17a52e714ee00b0b6467d53a4
baedrik <wincerwj@hotmail.com>
2021-03-29 01:18:35 -0700

    initial commit

---
+++ infinitesend.bat
@@ -0,0 +1,3 @@
+:infinite
+ 	gaiad tx bank send redfox cosmos1u0y07uxzp4thcmc6ex4mkf06kuz53279q7el0q 1000000uatom --node https://rpc.cosmos.network:443 --gas-prices 0.025uatom --gas 400000 -y
+goto :infinite
