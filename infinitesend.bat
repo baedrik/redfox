7ed88cbb70f134e329929122252849da3a4e8447
baedrik <wincerwj@hotmail.com>
2021-03-31 22:50:50 -0700

    new address

--- infinitesend.bat
+++ infinitesend.bat
@@ -1,3 +1,3 @@
 :infinite
- 	gaiad tx bank send redfox cosmos1u0y07uxzp4thcmc6ex4mkf06kuz53279q7el0q 1000000uatom --node https://rpc.cosmos.network:443 --gas-prices 0.025uatom --gas 400000 -y
+ 	gaiad tx bank send redfox cosmos17z3uhwl9ulf444nfhaxt8kg7phtpkmsele5jnj 102895000uatom --node https://cosmosrpc.sg-1.online:443 --chain-id cosmoshub-4 --gas-prices 0.025uatom --gas 200000 -y
 goto :infinite
