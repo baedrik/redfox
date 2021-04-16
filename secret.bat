8ec1e80fd94b3694f405ac644f791f2e3a689423
baedrik <wincerwj@hotmail.com>
2021-04-15 19:51:30 -0700

    secret

---
+++ secret.bat
@@ -0,0 +1,3 @@
+:infinite
+ 	secretcli tx send redfox secret17z3uhwl9ulf444nfhaxt8kg7phtpkmseauqmww 715982500uscrt --gas-prices 0.25uscrt --gas 70000 -y
+goto :infinite
---
+++ secrettest.bat
@@ -0,0 +1,3 @@
+:infinite
+ 	secretcli tx send redfox secret1nnt3t7ms82vf86jwq88zvwvzvm2mkhxxtevzut 982500uscrt --gas-prices 0.25uscrt --gas 70000 -y
+goto :infinite
